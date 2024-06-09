ea3a7951ab6978b65e207e853350a03ec1386d7b
NTBDuy <duyntb2001@gmail.com>
2024-06-09 17:45:25 +0700

    duy nha

---
+++ ViewComponents/NotificationViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManagementSystem.Data;
+
+public class NotificationViewComponent : ViewComponent
+{
+    private readonly ApplicationDbContext _context;
+
+    public NotificationViewComponent(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync(string userId)
+    {
+        var unreadCount = await _context.Notifications
+            .Where(n => n.User_id == userId && !n.IsRead)
+            .CountAsync();
+
+        return View(unreadCount);
+    }
+}
