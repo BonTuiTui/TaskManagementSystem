8ab7c066c572ea68859076fb9d0816d54d9666d6
NTBDuy <duyntb2001@gmail.com>
2024-06-19 10:56:47 +0700

    duy

--- obj/Debug/net7.0/TaskManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net7.0/TaskManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52950f28edd95e775948f6c98244eea2ef10d5cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
